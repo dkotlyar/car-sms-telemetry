a79b5fcab2d18575fd337e372b314ac940da37ab
Dmitry Kotlyar <dkotlyar@vsplatforma.ru>
2021-08-27 00:02:14 +0300

    implement download media rest methods

--- local_django/beacon_local/urls.py
+++ local_django/beacon_local/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('get_unpublished_snapshots', views.get_unpublished_snapshots),
     path('set_snapshots_statuses', views.set_snapshots_statuses),
+    path('get_unpublished_media', views.get_unpublished_media),
+    path('get_media_file/<int:id>', views.get_media_file),
 
     path('get_test', views.get_test),
     path('post_test', views.post_test)
