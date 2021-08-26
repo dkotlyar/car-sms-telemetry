532d4a5018939a7e047eee0af2a5208d6fcb1c78
Dmitry Kotlyar <33775104+dkotlyar@users.noreply.github.com>
2021-08-26 22:29:18 +0300

    implement local django rest methods

--- local_django/beacon_local/views.py
+++ local_django/beacon_local/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from beacon_local.models import Snapshot
+from beacon_local.utils import default_json_pagination, default_json_error, default_json_response, object_to_bool
 
 
 def get_test(request):
@@ -9,6 +12,38 @@ def get_test(request):
     return HttpResponse(status=200)
 
 
+@csrf_exempt
 def post_test(request):
     print(request.body)
-    return HttpResponse(status=200)
+    return default_json_response()
+
+
+def get_unpublished_snapshots(request):
+    snapshots = Snapshot.objects.filter(published=False)
+    return default_json_pagination(request, snapshots)
+
+
+@csrf_exempt
+def set_snapshots_statuses(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        if not ('ids' in body):
+            return default_json_error('Field `ids` not in request')
+        if not ('published' in body):
+            return default_json_error('Field `published` not in request')
+
+        snapshots = Snapshot.objects.filter(id__in=list(map(int, body['ids'])))
+        published = object_to_bool(body['published'])
+        ids = [s.id for s in snapshots]
+        snapshots.update(published=published)
+
+        return default_json_response({
+            'ids': ids,
+            'published': published
+        })
+
+    return HttpResponse(status=400)
+
+
+def get_unpublished_media(request):
+    pass
