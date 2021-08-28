225b9540ce84153bc6e96d7f3d740659cbd334a9
Dmitry Kotlyar <dkotlyar@vsplatforma.ru>
2021-08-28 17:00:01 +0300

    ??????????? rest ???????? ??? android

--- local_django/beacon_local/urls.py
+++ local_django/beacon_local/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('check_new_records', views.check_new_records),
     path('get_unpublished_snapshots', views.get_unpublished_snapshots),
     path('set_snapshots_statuses', views.set_snapshots_statuses),
     path('get_unpublished_media', views.get_unpublished_media),
